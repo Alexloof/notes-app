24c311e4736775155d95b4aaf9263e2b400489db
Alexloof <alex.loofet@gmail.com>
2017-04-12 14:29:32 +0200

    Added tests for notes

--- imports/api/users.test.js
+++ imports/api/users.test.js
@@ -14,7 +14,6 @@ if (Meteor.isServer) {
                 ]
             };
             const res = validateNewUser(testUser);
-
             expect(res).toBe(true);
         });
 
@@ -26,7 +25,6 @@ if (Meteor.isServer) {
                     }
                 ]
             };
-
             expect(() => {
                 validateNewUser(testUser);
             }).toThrow();
@@ -34,38 +32,4 @@ if (Meteor.isServer) {
     })
 
 }
-
-
-/*const add = (a, b) => {
-    if (typeof b !== "number") {
-        return a + a;
-    }
-
-    return a + b;
-}
-
-const square = (a) => a * a;
-
-describe("add", function () {
-    it("should add two numbers", function () {
-        const res = add(11, 9);
-
-        expect(res).toBe(20);
-    });
-
-    it("should double a single number", function () {
-        const res = add(44);
-
-        expect(res).toBe(88);
-    })
-});
-
-describe("square", function () {
-    it("should squara a number", function () {
-        const res = square(3);
-
-        expect(res).toBe(9);
-    });
-});*/
-
 
