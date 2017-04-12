303758468e8d28bc5c53d2870f77e5649b413ea5
Alexloof <alex.loofet@gmail.com>
2017-04-12 21:54:43 +0200

    Some more for NoteList

--- imports/ui/NoteList.js
+++ imports/ui/NoteList.js
@@ -5,15 +5,24 @@ import { createContainer } from "meteor/react-meteor-data";
 import { Notes } from "../api/notes";
 import NoteListHeader from "./NoteListHeader";
 import NoteListItem from "./NoteListItem";
+import NoteListEmptyItem from "./NoteListEmptyItem";
 
 export const NoteList = (props) => {
-    return(
+    const renderNotes = function () {
+        if (props.notes.length === 0) {
+            return <NoteListEmptyItem />;
+        } else {
+            return (
+                props.notes.map((note) => {
+                    return <NoteListItem key={note._id} note={note} />;
+                })
+            );
+        }
+    }
+    return (
         <div>
-            <NoteListHeader/>
-            {props.notes.map((note) => {
-                return <NoteListItem key={note._id} note={note}/>;
-            })}
-            NoteList { props.notes.length }
+            <NoteListHeader />
+            {renderNotes()}
         </div>
     );
 };
