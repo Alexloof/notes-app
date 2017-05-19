bf89d126121f60dc2d4e71ae9bfa63678e94242c
Alexloof <alex.loofet@gmail.com>
2017-05-19 23:06:27 +0200

    Added styles for sidebar

--- imports/ui/Editor.js
+++ imports/ui/Editor.js
@@ -45,7 +45,7 @@ export class Editor extends React.Component {
                     <input className="editor__title" value={this.state.title} placeholder="Title" onChange={this.handleTitleChange.bind(this)}/>
                     <textarea className="editor__body" value={this.state.body} placeholder="Your note here..." onChange={this.handleBodyChange.bind(this)}></textarea>
                     <div>
-                        <button className="button button-secondary" onClick={this.removeNote.bind(this)}>Delete Note</button>
+                        <button className="button button--secondary" onClick={this.removeNote.bind(this)}>Delete Note</button>
                     </div>  
                 </div>
             );
